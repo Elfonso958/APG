abe321cd977f36d87c811d25afd787c003d1e839
Elfonso958 <jayden.beck03@gmail.com>
2026-09-11 10:14:47 +1200

    Refresh cached Gantt assets

--- app/static/New_Gantt/crew_briefing_sw.js
+++ app/static/New_Gantt/crew_briefing_sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "crew-briefing-shell-v9";
+const CACHE_NAME = "crew-briefing-shell-v10";
 const SHELL_PATHS = [
   "/APG/dcs/crew-briefing",
-  "/APG/static/New_Gantt/live_gantt.css?v=crew-apg-1",
-  "/APG/static/New_Gantt/live_gantt.js?v=crew-apg-1",
+  "/APG/static/New_Gantt/live_gantt.css?v=crew-apg-2",
+  "/APG/static/New_Gantt/live_gantt.js?v=crew-apg-2",
   "/APG/static/New_Gantt/crew_briefing_icon.svg",
 ];
 
